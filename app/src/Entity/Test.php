897ce09948aca89697f7a7545ff08c76b494a2dd
Killian Biondo <biondo.killian@gmail.com>
2025-03-11 15:11:49 +0100

    add all files

---
+++ app/src/Entity/Test.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\TestRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TestRepository::class)]
+#[ApiResource]
+class Test
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
